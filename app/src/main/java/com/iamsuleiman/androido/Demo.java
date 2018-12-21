bfbccda3806ca12bc1b2d6b00f172957e7d4b68b
sai406 <saikumarbadapatla@gmail.com>
2018-12-21 12:50:45 +0530

    Update Demo.java (#1)

--- app/src/main/java/com/iamsuleiman/androido/Demo.java
+++ app/src/main/java/com/iamsuleiman/androido/Demo.java
@@ -29,7 +29,7 @@ public class Demo {
         return description;
     }
 
-    public void setDescription(String description) {
-        this.description = description;
-    }
+//     public void setDescription(String description) {
+//         this.description = description;
+//     }
 }
